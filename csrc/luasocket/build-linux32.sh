957d2e5683108d56e97ee0cedb4c3e70c0812462
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2013-12-24 16:44:06 +0200

    changed binary paths in build scripts

--- csrc/luasocket/build-linux32.sh
+++ csrc/luasocket/build-linux32.sh
@@ -1,2 +1,2 @@
 files="$(ls -1 src/*.c | grep -v wsocket)"
-gcc $files -O3 -s -o ../../linux/bin/socket_core.so -shared -I. -I../lua
+gcc $files -O3 -s -o ../../bin/linux32/socket_core.so -shared -I. -I../lua
