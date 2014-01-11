932dcb82b1e5a7900dac450edc17b7b626142d86
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-01-11 13:09:43 +0200

    moved dll to clib

--- csrc/socket/build-mingw32.sh
+++ csrc/socket/build-mingw32.sh
@@ -1,2 +1,2 @@
 files="$(ls -1 src/*.c | grep -v usocket)"
-gcc $files -O3 -s -o ../../bin/mingw32/socket_core.dll -shared -I. -I../lua -L../../bin/mingw32 -llua51 -lwsock32
+gcc $files -O3 -s -o ../../bin/mingw32/clib/socket_core.dll -shared -I. -I../lua -L../../bin/mingw32 -llua51 -lwsock32
