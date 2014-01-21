2713d393785efaf4014b938f3fd61cc4108c0c03
root <root@debian.local>
2014-01-20 20:04:36 -0500

    built on linux32 with GCC 4.4.5; fixed build script

--- csrc/socket/build-linux32.sh
+++ csrc/socket/build-linux32.sh
@@ -1,2 +1,2 @@
 files="$(ls -1 src/*.c | grep -v wsocket)"
-gcc $CFLAGS $files -shared -o ../../bin/linux32/socket_core.so -I. -I../lua
+gcc $CFLAGS $files -shared -o ../../bin/linux32/clib/socket_core.so -I. -I../lua
