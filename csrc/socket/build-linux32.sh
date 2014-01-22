3a3a8958e1146cfcc224e05f934d3f64438cfbad
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:52:48 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/socket/build-linux32.sh
+++ csrc/socket/build-linux32.sh
@@ -1,2 +1,2 @@
 files="$(ls -1 src/*.c | grep -v wsocket)"
-gcc $CFLAGS $files -shared -o ../../bin/linux32/clib/socket_core.so -I. -I../lua
+gcc -O2 -s -static-libgcc $files -shared -o ../../bin/linux32/clib/socket_core.so -I. -I../lua
--- csrc/socket/build-mingw32.sh
+++ csrc/socket/build-mingw32.sh
@@ -1,2 +1,2 @@
 files="$(ls -1 src/*.c | grep -v usocket)"
-gcc $CFLAGS $files -shared -o ../../bin/mingw32/clib/socket_core.dll -I. -I../lua -L../../bin/mingw32 -llua51 -lwsock32
+gcc -O2 -s -static-libgcc $files -shared -o ../../bin/mingw32/clib/socket_core.dll -I. -I../lua -L../../bin/mingw32 -llua51 -lwsock32
