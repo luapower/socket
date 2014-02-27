1950bce8e0eaa934a968e0ac8a44c094f2b3c5ba
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-02-27 02:16:58 +0000

    linux64 build

--- csrc/socket/build-linux64.sh
+++ csrc/socket/build-linux64.sh
@@ -1,2 +1,7 @@
 files="$(ls -1 src/*.c | grep -v wsocket)"
-gcc -O2 -s -static-libgcc -fPIC $files -shared -o ../../bin/linux64/clib/socket_core.so -I. -I../lua
+mkdir -p ../../bin/linux64/clib/socket
+gcc -O2 -s -static-libgcc -fPIC $files -shared -o ../../bin/linux64/clib/socket/core.so -I. -I../lua \
+    -DLUASOCKET_API=extern
+
+mkdir -p ../../bin/linux64/clib/mime
+gcc -O2 -s -static-libgcc -fPIC src/mime.c -shared -o ../../bin/linux64/clib/mime/core.so -I. -I../lua
