23e8666a5071b9fa1fde42f2699ee18df00ff7f4
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-02-27 02:17:30 +0000

    linux32 build

--- csrc/socket/build-linux32.sh
+++ csrc/socket/build-linux32.sh
@@ -1,2 +1,7 @@
-files="$(ls -1 src/*.c | grep -v wsocket)"
-gcc -O2 -s -static-libgcc $files -shared -o ../../bin/linux32/clib/socket_core.so -I. -I../lua
+files="$(ls -1 src/*.c | grep -v "wsocket\|serial")"
+mkdir -p ../../bin/linux32/clib/socket
+gcc -O2 -s -static-libgcc $files -shared -o ../../bin/linux32/clib/socket/core.so -I. -I../lua \
+    -DLUASOCKET_API=extern
+
+mkdir -p ../../bin/linux32/clib/mime
+gcc -O2 -s -static-libgcc src/mime.c -shared -o ../../bin/linux32/clib/mime/core.so -I. -I../lua
