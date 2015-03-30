21e14963e50a498e0581e35e4b4170eea0c7fb92
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-03-30 17:03:38 +0300

    unimportant

--- csrc/socket/build.sh
+++ csrc/socket/build.sh
@@ -1,11 +1,11 @@
-mkdir -p "$(dirname ../../bin/$P/clib/$SD)"
+mkdir -p "$(dirname ../../bin/$P/clib/socket/$SD)"
 gcc -c -O2 $C $files -I. -I../lua
-gcc *.o -shared -o ../../bin/$P/clib/$SD $L
+gcc *.o -shared -o ../../bin/$P/clib/socket/$SD $L
 ar rcs ../../bin/$P/$SA *.o
 
-mkdir -p "$(dirname ../../bin/$P/clib/$MD)"
+mkdir -p "$(dirname ../../bin/$P/clib/mime/$MD)"
 gcc -c -O2 $C src/mime.c -I. -I../lua
-gcc mime.o -shared -o ../../bin/$P/clib/$MD $L
+gcc mime.o -shared -o ../../bin/$P/clib/mime/$MD $L
 ar rcs ../../bin/$P/$MA mime.o
 
 rm *.o
