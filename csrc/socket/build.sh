e8c45977ded768188e8a223b66850af480de1640
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-04-12 04:44:47 +0300

    renamed lua -> lua-headers

--- csrc/socket/build.sh
+++ csrc/socket/build.sh
@@ -1,10 +1,10 @@
 mkdir -p "$(dirname ../../bin/$P/clib/socket/$SD)"
-gcc -c -O2 $C $files -I. -I../lua
+gcc -c -O2 $C $files -I. -I../lua-headers
 gcc *.o -shared -o ../../bin/$P/clib/socket/$SD $L
 ar rcs ../../bin/$P/$SA *.o
 
 mkdir -p "$(dirname ../../bin/$P/clib/mime/$MD)"
-gcc -c -O2 $C src/mime.c -I. -I../lua
+gcc -c -O2 $C src/mime.c -I. -I../lua-headers
 gcc mime.o -shared -o ../../bin/$P/clib/mime/$MD $L
 ar rcs ../../bin/$P/$MA mime.o
 
